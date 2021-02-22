b6afd9ac373896f60b90877256ffca8f612d2f02
gahee.han <gahee.han@streami.co>
2021-02-22 17:59:42 +0900

    Initial Commit

---
+++ app/src/main/java/com/augustchic/rainbow/ui/auth/AuthActivity.kt
@@ -0,0 +1,21 @@
+package com.augustchic.rainbow.ui.auth
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.augustchic.rainbow.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
